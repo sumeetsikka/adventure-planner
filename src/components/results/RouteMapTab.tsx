35017ebb865bcf0d59a0839eda2c51bcf86bbfce
sumeetsikka <270227038+sumeetsikka@users.noreply.github.com>
2026-04-29 21:44:24 +1000

    Auto-commit: file changes

--- src/components/results/RouteMapTab.tsx
+++ src/components/results/RouteMapTab.tsx
@@ -10,7 +10,7 @@ import markerIcon2x from 'leaflet/dist/images/marker-icon-2x.png';
 import markerIcon from 'leaflet/dist/images/marker-icon.png';
 import markerShadow from 'leaflet/dist/images/marker-shadow.png';
 
-delete (L.Icon.Default.prototype as any)._getIconUrl;
+delete (L.Icon.Default.prototype as { _getIconUrl?: unknown })._getIconUrl;
 L.Icon.Default.mergeOptions({
   iconRetinaUrl: markerIcon2x,
   iconUrl: markerIcon,
